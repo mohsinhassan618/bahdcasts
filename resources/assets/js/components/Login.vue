de8e43df6a00da2da744e3d9415a9e86011587de
mohsinhassan618 <mohsin.hassan618@gmail.com>
2018-08-15 14:08:04 +0500

    Initial commit

---
+++ resources/assets/js/components/Login.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="modal fade" id="LoginModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+
+            <div class="card card-shadowed p-50 w-400 mb-0" style="max-width: 100%">
+                <h5 class="text-uppercase text-center">Login</h5>
+                <br><br>
+
+                <form>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email" v-model="email">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password" v-model="password">
+                    </div>
+
+                    <div class="form-group flexbox py-10">
+                        <label class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" v-model="remember">
+                            <span class="custom-control-indicator"></span>
+                            <span class="custom-control-description">Remember me</span>
+                        </label>
+
+                        <a class="text-muted hover-primary fs-13" href="#">Forgot password?</a>
+                    </div>
+
+                    <div class="form-group">
+                        <button class="btn btn-bold btn-block btn-primary" v-on:click="attemptLogin" type="submit" :disabled="!isValidLoginForm">
+                            Login
+                        </button>
+                    </div>
+                </form>
+
+
+                <hr class="w-30">
+
+                <p class="text-center text-muted fs-13 mt-20">Don't have an account? <a href="page-register.html">Sign up</a>
+                </p>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data(){
+            return {
+                email: "",
+                password: "",
+                remember: true,
+            }
+        },
+
+        methods: {
+            emailIsValid(){
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) {
+                    return (true);
+                } else {
+                    return false;
+                }
+            },
+            attemptLogin(){
+                axios.post('/login',{
+                    email: this.email ,password:this.password ,remember:this.remember
+                })
+
+
+            }
+
+        },
+
+        computed: {
+            isValidLoginForm(){
+                return this.emailIsValid() && this.password
+            }
+        }
+    }
+</script>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    {{--<title>{{ config('app.name', 'Laravel') }}</title>--}}
+    <title>The SaaS - Header Image</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/core.min.css') }}"     rel="stylesheet">
+    <link href="{{ asset('css/thesaas.min.css') }}"  rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}"        rel="stylesheet">
+    {{--<link href="{{ asset('css/app.css') }}"          rel="stylesheet">--}}
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="{{ asset('img/apple-touch-icon.png') }}">
+    <link rel="icon" href="{{ asset('img/favicon.png') }}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+</head>
+<body>
+    <div id="app">
+
+
+        <!-- Topbar -->
+        <nav class="topbar topbar-inverse topbar-expand-md topbar-sticky">
+            <div class="container">
+
+                <div class="topbar-left">
+                    <button class="topbar-toggler">&#9776;</button>
+                    <a class="topbar-brand" href="">
+                        <img class="logo-default" src="{{ asset('img/logo.png') }}" alt="logo">
+                        <img class="logo-inverse" src="{{ asset('img/logo-light.png') }}" alt="logo">
+                    </a>
+                </div>
+
+
+                <div class="topbar-right">
+                    <ul class="topbar-nav nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="javascript:;" data-toggle="modal" data-target="#LoginModel" >Login</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+        <!-- END Topbar -->
+
+
+        <!-- Header -->
+        @yield('header')
+        <!-- END Header -->
+
+
+
+
+        <!-- Main container -->
+        <main class="main-content">
+            @yield('content')
+        </main>
+        <!-- END Main container -->
+
+
+        <!-- Footer -->
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row gap-y align-items-center">
+                    <div class="col-12 col-lg-6">
+                        <ul class="nav nav-primary nav-hero">
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- END Footer -->
+
+        <vue-login></vue-login>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/core.min.js') }}"></script>
+    <script src="{{ asset('js/thesaas.min.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+    @yield('footer-scripts')
+</body>
+</html>
